ab9519407910d0af29600a9cc37f909fcb38e804
danielArami <daniel18arami@gmail.com>
2019-06-16 11:21:35 +0100

    picture added

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,7 +9,7 @@ const Button = (props) => {
         <TouchableOpacity onPress = {onPress}>
         <View style = {{ height: height, width: width }}>
             <ImageBackground
-                style = {{ height: '100%', width: '100%', alignItems: 'center', justifyContent: 'center'}}
+                style = {{ height: '100%', width: '105%', alignItems: 'center', justifyContent: 'center'}}
                 source = {{uri: 'http://www.clker.com/cliparts/n/h/S/s/F/k/square-button-clear-md.png'}}>
                 <View style = {{height: '70%', width: '70%', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'}}>
                     <Image
